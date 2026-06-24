0499e2acbd08484b46d6056152371d3f916aa55d
Langsdorff <131063968+clangsdorff@users.noreply.github.com>
2026-06-24 05:35:43 +0300

    unica: add a14 bootanim mod support

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -1,5 +1,5 @@
 TWOTHREE_TARGETS="m34x"
-TWOFOUR_TARGETS="a52sxq a51 a53x a73xq m52xq r8q r9q r9q2"
+TWOFOUR_TARGETS="a52sxq a51 a53x a73xq m52xq r8q r9q r9q2 a14"
 
 if echo "$TWOTHREE_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
     cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/2340x1080/"* "$WORK_DIR/system/system/media"
