43ec855caa2277e0b9a5ce312ae9fd563f4e0ab8
Langsdorff <131063968+clangsdorff@users.noreply.github.com>
2026-06-24 05:33:00 +0300

    unica: add api level 35 support

--- unica/patches/vndk/customize.sh
+++ unica/patches/vndk/customize.sh
@@ -14,6 +14,9 @@ ADD_TARGET_VNDK_APEX() {
         "33")
             ADD_TO_WORK_DIR "dm3qxxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
             ;;
+        "34")
+            ADD_TO_WORK_DIR "r12sxxx" "system_ext" "apex/com.android.vndk.v34.apex" 0 0 644 "u:object_r:system_file:s0"
+            ;;
         *)
             ABORT "No APEX blob available for VNDK $TARGET_VENDOR_API_LEVEL"
             ;;
@@ -28,16 +31,16 @@ if [[ "$SOURCE_VENDOR_API_LEVEL" != "$TARGET_VENDOR_API_LEVEL" ]]; then
         SYS_EXT_DIR="$WORK_DIR/system/system/system_ext"
     fi
 
-    if [ "$SOURCE_VENDOR_API_LEVEL" -gt "34" ] && [ "$TARGET_VENDOR_API_LEVEL" -gt "34" ]; then
+    if [ "$SOURCE_VENDOR_API_LEVEL" -gt "35" ] && [ "$TARGET_VENDOR_API_LEVEL" -gt "35" ]; then
         :
-    elif [ "$SOURCE_VENDOR_API_LEVEL" -gt "34" ] && [ "$TARGET_VENDOR_API_LEVEL" -le "34" ]; then
+    elif [ "$SOURCE_VENDOR_API_LEVEL" -gt "35" ] && [ "$TARGET_VENDOR_API_LEVEL" -le "35" ]; then
         ADD_TARGET_VNDK_APEX
         sed -i '$d' "$SYS_EXT_DIR/etc/vintf/manifest.xml"
         echo "    <vendor-ndk>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
         echo "        <version>$TARGET_VENDOR_API_LEVEL</version>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
         echo "    </vendor-ndk>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
         echo "</manifest>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
-    elif [ "$SOURCE_VENDOR_API_LEVEL" -le "34" ] && [ "$TARGET_VENDOR_API_LEVEL" -gt "34" ]; then
+    elif [ "$SOURCE_VENDOR_API_LEVEL" -le "35" ] && [ "$TARGET_VENDOR_API_LEVEL" -gt "35" ]; then
         DELETE_FROM_WORK_DIR "system_ext" "apex/com.android.vndk.v$SOURCE_VENDOR_API_LEVEL.apex"
         sed -i -e "/vendor-ndk/d" -e "/version>/d" "$SYS_EXT_DIR/etc/vintf/manifest.xml"
     elif [ ! -f "$SYS_EXT_DIR/apex/com.android.vndk.v$TARGET_VENDOR_API_LEVEL.apex" ]; then
