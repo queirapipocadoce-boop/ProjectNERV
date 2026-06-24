37bae966deb835c2a46f4f07242f150f6deef4de
Langsdorff <131063968+clangsdorff@users.noreply.github.com>
2026-06-24 05:31:44 +0300

    target: remove a53x, m34x and m52xq to fix filename errors

--- target/m52xq/patches/miscs/customize.sh
+++
@@ -1,6 +0,0 @@
-LOG_STEP_IN "- Fixing up /product/etc/build.prop"
-sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
-    && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/product/etc/build.prop" \
-    && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
-    && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/product/etc/build.prop"
-LOG_STEP_OUT
--- target/m52xq/patches/rio/customize.sh
+++
@@ -1,8 +0,0 @@
-rm -f "$WORK_DIR/kernel/"*.img
-
-LOG "- Replacing kernel binaries with Rio (Vanilla)"
-cp -a "$SRC_DIR/prebuilts/kernels/m52xq/"* "$WORK_DIR/kernel"
-
-LOG_STEP_IN "- Removing kernel modules in vendor"
-DELETE_FROM_WORK_DIR "vendor" "lib/modules"
-LOG_STEP_OUT
